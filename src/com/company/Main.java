801c29092b3c19b6654c75674046e92f13af5224
Todd <tk304@pvsd.org>
2018-09-11 21:47:28 -0400

    Oh, say can you see by the dawn's early light What so proudly we hailed at the twilight's last gleaming? Whose broad stripes and bright stars through the perilous fight, O'er the ramparts we watched were so gallantly streaming? And the rocket's red glare, the bombs bursting in air, Gave proof through the night that our flag was still there. Oh, say does that star-spangled banner yet wave O'er the land of the free and the home of the brave? On the shore, dimly seen through the mists of the deep, Where the foe's haughty host in dread silence reposes, What is that which the breeze, o'er the towering steep, As it fitfully blows, half conceals, half discloses? Now it catches the gleam of the morning's first beam, In full glory reflected now shines in the stream: 'Tis the star-spangled banner! Oh long may it wave O'er the land of the free and the home of the brave! And where is that band who so vauntingly swore That the havoc of war and the battle's confusion, A home and a country should leave us no more! Their blood has washed out their foul footsteps' pollution. No refuge could save the hireling and slave From the terror of flight, or the gloom of the grave: And the star-spangled banner in triumph doth wave O'er the land of the free and the home of the brave! Oh! thus be it ever, when freemen shall stand Between their loved home and the war's desolation! Blest with victory and peace, may the heav'n rescued land Praise the Power that hath made and preserved us a nation. Then conquer we must, when our cause it is just, And this be our motto: "In God is our trust." And the star-spangled banner in triumph shall wave O'er the land of the free and the home of the brave!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,91 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        double num1 = 0, num2 = 0;
+        String op;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Would you like to add, subtract, multiple, or divide?");
+        op = scan.nextLine();
+
+        String buff = null;
+
+        switch (op.toLowerCase())
+        {
+            case "add":
+                buff = "sum";
+                break;
+            case "subtract":
+                buff = "difference";
+                break;
+            case "multiply":
+                buff = "product";
+                break;
+            case "divide":
+                buff = "quotient";
+                break;
+            default:
+                System.out.println("Invalid expression");
+        }
+
+        System.out.println("Enter your first number");
+        try {
+            num1 = scan.nextInt();
+        } catch (Exception ex)
+        {
+            System.out.println("Error: Input was not an integer.");
+        }
+
+        System.out.println("Enter your second number");
+        try {
+            num2 = scan.nextInt();
+        } catch (Exception ex)
+        {
+            System.out.println("Error: Input was not an integer.");
+        }
+
+        String satisfaction;
+        System.out.println("The " + buff + " of your two numbers is " + (Math.sqrt(num1 + num2)) + ". Are you satisfied?");
+
+        satisfaction = scan.nextLine(); // FIX ME
+        satisfaction = scan.nextLine(); // Putting this twice to fix WTJ
+
+        boolean runPrompt = false;
+
+        switch (satisfaction.toLowerCase())
+        {
+            case "yes":
+                runPrompt = false;
+                break;
+            case "no":
+                runPrompt = true;
+                break;
+        }
+
+        if (runPrompt)
+        {
+            String ans;
+            System.out.println("Can you identify what math class method I ran the real answer through?");
+            ans = scan.nextLine();
+
+            switch (ans.toLowerCase())
+            {
+                case "sqrt":
+                    System.out.println("Ahh, you got me. Now go away, I'm annoyed that I lost.");
+                    break;
+                default:
+                    System.out.println("Haha wrong, I used sqrt");
+            }
+        }
+        else
+        {
+            System.out.println("Haha! So you see, I pulled a fast one on you! Now you've been embarrassed from a computer.");
+        }
+
     }
 }
